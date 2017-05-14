5c6b705412247fc9be8d12d06d8ccbc419c0a3be
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-05-14 23:10:19 +0200

    Move single remaining validator into patch class

--- lib/Patch.php
+++ lib/Patch.php
@@ -83,7 +83,7 @@ class Patch implements JsonSerializable
 
     public function insert($at, $selector, $items)
     {
-        Validators::validateInsert($at, $selector, $items);
+        $this->validateInsert($at, $selector, $items);
         return $this->assign('insert', [$at => $selector, 'items' => $items]);
     }
 
@@ -142,5 +142,35 @@ class Patch implements JsonSerializable
     {
         $this->operations = [];
         return $this;
+    }
+
+    /**
+     * Validates parameters for an insert operation
+     *
+     * @param string $at Location at which to insert
+     * @param string $selector Selector to match at
+     * @param array $items Array items to insert
+     * @throws InvalidArgumentException
+     */
+    private function validateInsert($at, $selector, $items)
+    {
+        $insertLocations = ['before', 'after', 'replace'];
+        $signature = 'insert(at, selector, items)';
+
+        $index = array_search($at, $insertLocations);
+        if ($index === false) {
+            $valid = implode(', ', array_map(function ($loc) {
+                return '"' . $loc . '"';
+            }, $insertLocations));
+            throw new InvalidArgumentException($signature . ' takes an "at"-argument which is one of: ' . $valid);
+        }
+
+        if (!is_string($selector)) {
+            throw new InvalidArgumentException($signature . ' takes a "selector"-argument which must be a string');
+        }
+
+        if (!is_array($items)) {
+            throw new InvalidArgumentException($signature . ' takes an "items"-argument which must be an array');
+        }
     }
 }
